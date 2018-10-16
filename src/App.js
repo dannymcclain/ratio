8fb7af8288d5d94f9bde38a32b7a8937987bfee8
Danny <dannyxmcclain@gmail.com>
2018-10-16 14:55:39 -0500

    updated functionality

--- src/App.js
+++ src/App.js
@@ -3,66 +3,31 @@ import './App.css';
 
 class App extends Component {
   state = {
-    ratio: '',
-    value1: '',
-    value2: ''
+    term1: '',
+    term2: '',
+    width: '',
+    height: ''
   };
 
-  // handleChange = event => {
-  //   const { value, name } = event.target;
-  //   this.setState(
-  //     {
-  //       [name]: value
-  //     },
-  //     this.setValues
-  //   );
-  // };
-
-  // setValues = () => {
-  //   const numerator = 4;
-  //   const denominator = 3;
-  //   const v1 = (numerator * this.state.value2) / denominator;
-  //   const v2 = (denominator * this.state.value1) / numerator;
-  //   this.setState({
-  //     value1: v1,
-  //     value2: v2
-  //   });
-  // };
-
-  changeRatio = event => {
+  handleChange = event => {
+    const { value, name } = event.target;
     this.setState(
       {
-        ratio: event.target.value
+        [name]: value
       },
-      this.setValues
+      this.findValue
     );
   };
 
-  setValues = () => {
-    const ratioValues = this.state.ratio.split(':');
-    const ratioNum = ratioValues[0];
-    const ratioDen = ratioValues[1];
-    // const v1 = (ratioNum * this.state.value2) / ratioDen;
-    const v2 = (ratioDen * this.state.value1) / ratioNum;
-    console.log(v2);
-    this.setState({
-      value2: v2
-    });
+  findValue = () => {
+    if (this.state.term1 && this.state.term2 && this.state.width) {
+      const height = (this.state.term2 * this.state.width) / this.state.term1;
+      this.setState({
+        height: height
+      });
+    }
   };
 
-  changeValue1 = event => {
-    this.setState(
-      {
-        value1: event.target.value
-      },
-      this.setValues
-    );
-  };
-  changeValue2 = event => {
-    this.setState({
-      value2: event.target.value
-    });
-  };
   render() {
     return (
       <div className="App">
@@ -71,29 +36,38 @@ class App extends Component {
         </header>
         <section className="preview" />
         <section className="controls">
-          <select
-            name="ratio"
-            value={this.state.value}
-            onChange={this.changeRatio}
-          >
-            <option value="1:1">1:1</option>
-            <option value="4:3">4:3</option>
-            <option value="3:2">3:2</option>
-            <option value="8:5">8:5</option>
-            <option value="5:3">5:3</option>
-            <option value="16:9">16:9</option>
-            <option value="2:1">2:1</option>
-          </select>
-          <input
-            name="value1"
-            value={this.state.value1}
-            onChange={this.changeValue1}
-          />
-          <input
-            name="value2"
-            value={this.state.value2}
-            onChange={this.changeValue2}
-          />
+          <div className="terms">
+            <label>Ratio A</label>
+            <input
+              name="term1"
+              type="number"
+              value={this.state.term1}
+              onChange={this.handleChange}
+            />
+            <label>Ratio B</label>
+            <input
+              name="term2"
+              type="number"
+              value={this.state.term2}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="values">
+            <label>Width</label>
+            <input
+              name="width"
+              type="number"
+              value={this.state.width}
+              onChange={this.handleChange}
+            />
+            <label>Height</label>
+            <input
+              name="height"
+              type="number"
+              value={this.state.height}
+              disabled={true}
+            />
+          </div>
         </section>
       </div>
     );
