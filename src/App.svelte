2d3de63e867861eeb906cd0187de865a5340a99c
Danny McClain <dannyxmcclain@gmail.com>
2020-03-06 11:39:10 -0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+  let ratioA = 4;
+  let ratioB = 3;
+  let resultA = 1200;
+  let resultB = 900;
+  let roundResult = false;
+  function calculateResult() {
+    // if (roundResult) {
+    //   resultB = Math.round((resultA * ratioB) / ratioA);
+    // } else {
+    //   resultB = (resultA * ratioB) / ratioA;
+    // }
+    resultB = roundResult
+      ? Math.round((resultA * ratioB) / ratioA)
+      : (resultA * ratioB) / ratioA;
+  }
+</script>
+
+<style>
+
+</style>
+
+<main>
+  <input
+    size="1"
+    type="number"
+    bind:value={ratioA}
+    on:input={calculateResult} />
+  <input
+    size="1"
+    type="number"
+    bind:value={ratioB}
+    on:input={calculateResult} />
+  <input
+    size="1"
+    type="number"
+    bind:value={resultA}
+    on:input={calculateResult} />
+  <input size="1" type="number" bind:value={resultB} disabled />
+
+  <label>
+
+    <input
+      type="checkbox"
+      bind:checked={roundResult}
+      on:change={calculateResult} />
+    Round to nearest whole number
+  </label>
+</main>
