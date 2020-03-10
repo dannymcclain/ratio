584104b1f18710177222405febe0975fc83392b2
Danny McClain <dannyxmcclain@gmail.com>
2020-03-10 11:42:10 -0500

    styling updates

--- src/App.svelte
+++ src/App.svelte
@@ -28,22 +28,38 @@
 </script>
 
 <style>
+  :root {
+    --color-accent: #ffbfea;
+  }
+  :global(body) {
+    color: #000;
+    background: var(--color-accent);
+    box-sizing: border-box;
+    font-family: "Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  }
+
   main {
     height: 100vh;
     overflow: hidden;
     position: relative;
     padding: 63px;
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+    align-items: flex-start;
   }
   .form {
-    width: 100vw;
-    height: 100vh;
+    margin-bottom: 36px;
   }
-  .form__label {
-    font-size: 27px;
-    line-height: 30px;
+  .form__label,
+  .form__input--checkbox > span {
+    margin-bottom: 9px;
     font-weight: bold;
-    letter-spacing: -0.03em;
-    margin-bottom: 18px;
+    font-size: 12px;
+    line-height: 14px;
+    letter-spacing: 0.09em;
+    text-transform: uppercase;
   }
   .form__divider {
     font-weight: 800;
@@ -88,14 +104,13 @@
 
   .form__input--checkbox {
     display: flex;
+    justify-content: flex-start;
+    align-items: center;
     cursor: pointer;
     position: relative;
   }
   .form__input--checkbox > span {
-    font-weight: 500;
-    font-size: 18px;
-    line-height: 30px;
-    letter-spacing: -0.03em;
+    margin-bottom: 6px;
   }
   .form__input--checkbox > input {
     height: 27px;
@@ -122,10 +137,10 @@
     content: "\2713";
     display: block;
     text-align: center;
-    color: #49f265;
+    color: var(--color-accent);
     position: absolute;
     left: 4px;
-    top: -1px;
+    top: 6px;
   }
 
   .info {
@@ -138,78 +153,141 @@
     height: 100vh;
     background: #000;
     color: #fff;
-    padding: 63px;
+    /* padding: 63px; */
     transform: translateY(50vh);
     transition: transform 300ms cubic-bezier(0.075, 0.82, 0.165, 1),
       opacity 200ms linear;
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+    align-items: flex-start;
   }
   .info--open {
     opacity: 1;
     pointer-events: auto;
     transform: translateY(0);
   }
+  .info .form__label {
+    color: var(--color-accent);
+  }
+  .info .form__label a {
+    color: var(--color-accent);
+    text-decoration: underline;
+  }
+  .info .form__label a:hover {
+    color: #fff;
+  }
   .toggle {
-    /* padding: 18px; */
-    font-weight: 400;
-    font-size: 36px;
+    font-weight: bold;
+    font-size: 12px;
+    line-height: 14px;
+    letter-spacing: 0.09em;
+    text-transform: uppercase;
+    padding: 7px 9px;
+    background: transparent;
+    outline: none;
+    border-radius: 0;
     cursor: pointer;
-    display: inline-block;
+    transition: color 100ms linear, background 100ms linear;
+  }
+  .toggle--info {
+    border: 2px solid #000;
+    color: #000;
+  }
+  .toggle--info:hover {
+    background: #000;
+    color: var(--color-accent);
+  }
+  .toggle--close {
+    border: 2px solid var(--color-accent);
+    color: var(--color-accent);
+  }
+  .toggle--close:hover {
+    background: var(--color-accent);
+    color: #000;
+  }
+
+  .title {
+    font-weight: 800;
+    font-size: 81px;
+    line-height: 90px;
+    letter-spacing: -0.03em;
+    color: var(--color-accent);
+    max-width: 691px;
+    margin-top: 18px;
+  }
+  .info__wrap {
+    height: 100vh;
+    padding: 63px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
   }
 </style>
 
 <main>
-  <section class="form">
-    <p class="form__label">Ratio</p>
-    <input
-      placeholder=""
-      class="form__input--number"
-      size="1"
-      type="number"
-      bind:value={ratioA}
-      on:input={focusResultA ? calculateA() : calculateB()} />
-    <p class="form__divider">:</p>
-    <input
-      placeholder=""
-      class="form__input--number"
-      size="1"
-      type="number"
-      bind:value={ratioB}
-      on:input={focusResultA ? calculateA() : calculateB()} />
-    <p class="form__label">Result</p>
-    <input
-      placeholder=""
-      class="form__input--number"
-      size="1"
-      type="number"
-      bind:value={resultA}
-      on:input={calculateB} />
-    <p class="form__divider">:</p>
-    <input
-      placeholder=""
-      class="form__input--number"
-      size="1"
-      type="number"
-      bind:value={resultB}
-      on:input={calculateA} />
-
-    <label class="form__input--checkbox">
+  <button class="toggle toggle--info" on:click={toggleInfo}>Info</button>
+  <div class="wrap">
+    <section class="form">
+      <p class="form__label">Ratio</p>
       <input
-        class="form__input--checkbox__input"
-        type="checkbox"
-        bind:checked={roundResult}
-        on:change={focusResultA ? calculateA() : calculateB()} />
-      <span>Round to nearest whole number</span>
-    </label>
-    <p class="toggle" on:click={toggleInfo}></p>
-  </section>
-  <section class="info {infoOpen ? 'info--open' : ''}">
-    <p class="toggle" on:click={toggleInfo}></p>
-    <h1>Ratio calculator.</h1>
-    <p>A simple aspect ratio/proportion calculator.</p>
-    <p>
+        placeholder=""
+        class="form__input--number"
+        size="1"
+        type="number"
+        bind:value={ratioA}
+        on:input={focusResultA ? calculateA() : calculateB()} />
+      <p class="form__divider">:</p>
+      <input
+        placeholder=""
+        class="form__input--number"
+        size="1"
+        type="number"
+        bind:value={ratioB}
+        on:input={focusResultA ? calculateA() : calculateB()} />
+    </section>
+    <section class="form">
+      <p class="form__label">Result</p>
+      <input
+        placeholder=""
+        class="form__input--number"
+        size="1"
+        type="number"
+        bind:value={resultA}
+        on:input={calculateB} />
+      <p class="form__divider">:</p>
+      <input
+        placeholder=""
+        class="form__input--number"
+        size="1"
+        type="number"
+        bind:value={resultB}
+        on:input={calculateA} />
+    </section>
+
+    <section class="form">
+      <label class="form__input--checkbox">
+        <input
+          class="form__input--checkbox__input"
+          type="checkbox"
+          bind:checked={roundResult}
+          on:change={focusResultA ? calculateA() : calculateB()} />
+        <span>Round to nearest whole number</span>
+      </label>
+    </section>
+  </div>
+</main>
+<section class="info {infoOpen ? 'info--open' : ''}">
+  <button class="toggle toggle--close" on:click={toggleInfo}>Close</button>
+  <div class="info__wrap">
+    <section class="content">
+      <h1 class="form__label">Ratio Calculator</h1>
+      <p class="title">A simple aspect ratio/proportion calculator.</p>
+    </section>
+    <p class="form__label">
       Created by
       <a href="https://dannymcclain.com">Danny McClain</a>
-      .
     </p>
-  </section>
-</main>
+  </div>
+</section>
